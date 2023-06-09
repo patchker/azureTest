ffd270326e6af7e8e8348f70e066c7c02c2308c3
patchker <paczker@icloud.com>
2023-06-10 01:32:04 +0200

    testowy azure6

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
 import os
-
 from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+                   send_from_directory, url_for, session)
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'  # Remember to set a secret key
 
+SECRET_CODE = 'your_secret_code'  # Set your secret code
 
 @app.route('/')
 def index():
-   print('Request for index page received')
-   return render_template('index.html')
+   if 'code' in session and session['code'] == SECRET_CODE:
+       print('Request for index page received')
+       return render_template('index.html')
+   else:
+       return redirect(url_for('enter_code'))
+
+@app.route('/enter_code', methods=['GET', 'POST'])
+def enter_code():
+    if request.method == 'POST':
+        code = request.form.get('code')
+        if code == SECRET_CODE:
+            session['code'] = code
+            return redirect(url_for('index'))
+        else:
+            return 'Invalid code', 403
+    else:
+        return render_template('enter_code.html')
 
 @app.route('/favicon.ico')
 def favicon():
@@ -18,15 +34,17 @@ def favicon():
 
 @app.route('/hello', methods=['POST'])
 def hello():
-   name = request.form.get('name')
+    if 'code' not in session or session['code'] != SECRET_CODE:
+        return 'Unauthorized', 403
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+    name = request.form.get('name')
 
+    if name:
+        print('Request for hello page received with name=%s' % name)
+        return render_template('hello.html', name = name)
+    else:
+        print('Request for hello page received with no name or blank name -- redirecting')
+        return redirect(url_for('index'))
 
 if __name__ == '__main__':
    app.run()
