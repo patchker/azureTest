86d8cc712dc694f0451f9f6c6e064e54ee408945
patchker <paczker@icloud.com>
2023-06-10 00:26:20 +0200

    testowy azure4

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from flask import Flask
-app = Flask(__name__)
+import flask
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app = flask.Flask(__name__)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+@app.route('/', methods=['GET'])
+def home():
+    return '<h1>Hello!</h1></p>'
+
+# debug app:
+# app.run()
+
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
